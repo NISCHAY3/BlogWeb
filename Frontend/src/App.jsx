a28027eb4fb8fcff32e61d369ac643a983ff8e34
Nischay <barejanischay5@gmail.com>
2024-09-22 18:49:52 +0530

    started

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import Login from './components/account/Login'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <Login></Login>
+
+    </>
+  )
+}
+
+export default App
