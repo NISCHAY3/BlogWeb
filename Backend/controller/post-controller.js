9ec60730704ccec6a1d421e4db6c63f3d05d1ff3
Nischay <barejanischay5@gmail.com>
2024-10-04 15:39:32 +0530

    errors fixed

--- Backend/controller/post-controller.js
+++ Backend/controller/post-controller.js
@@ -14,19 +14,19 @@ export const createPost = async (request, response) => {
 }
 
 // export const updatePost = async (request, response) => {
-try {
-    const post = await Post.findById(request.params.id);
+// try {
+//     const post = await Post.findById(request.params.id);
 
-    if (!post) {
-        response.status(404).json({ msg: 'Post not found' })
-    }
+//     if (!post) {
+//         response.status(404).json({ msg: 'Post not found' })
+//     }
 
-    await Post.findByIdAndUpdate(request.params.id, { $set: request.body })
+//     await Post.findByIdAndUpdate(request.params.id, { $set: request.body })
 
-    response.status(200).json('post updated successfully');
-} catch (error) {
-    response.status(500).json(error);
-}
+//     response.status(200).json('post updated successfully');
+// } catch (error) {
+//     response.status(500).json(error);
+// }
 // }
 
 // export const deletePost = async (request, response) => {
